3df8ff3ae9b887c992fa0f33c59d73e9c6fd1727
Netanel Mülbert <netanelmul@gmail.com>
2023-05-30 19:16:34 +0300

    Uncommented some sections back

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,31 @@
 import Factories.ShapeFactory;
 import Shapes.Interfaces.Shape;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-//        MyClass firstClass = new MyClass();
-//        firstClass.run();
-//
-//          Scanner myScanner = new Scanner(System.in);
-//        String a,b;
-//        System.out.println("Enter a word:");
-//        a = myScanner.next();
-//
-//        System.out.println("Enter another word:");
-//        b = myScanner.next();
-//
-//        System.out.println(firstClass.AddStrings(a,b));
-//
-//        MySecondClass secondClass1 = new MySecondClass(1);
-//        MySecondClass secondClass2 = new MySecondClass(2,3);
-//        MySecondClass secondClass3 = new MySecondClass(4,5);
-//        MySecondClass secondClass4 = new MySecondClass(6);
-//
-//        System.out.println("Enter a number:");
-//        System.out.println(MySecondClass.DoubleMe(myScanner.nextInt()));
+        MyClass firstClass = new MyClass();
+        firstClass.run();
+
+        Scanner myScanner = new Scanner(System.in);
+        String a,b;
+        System.out.println("Enter a word:");
+        a = myScanner.next();
+
+        System.out.println("Enter another word:");
+        b = myScanner.next();
+
+        System.out.println(firstClass.AddStrings(a,b));
+
+        MySecondClass secondClass1 = new MySecondClass(1);
+        MySecondClass secondClass2 = new MySecondClass(2,3);
+        MySecondClass secondClass3 = new MySecondClass(4,5);
+        MySecondClass secondClass4 = new MySecondClass(6);
+
+        System.out.println("Enter a number:");
+        System.out.println(MySecondClass.DoubleMe(myScanner.nextInt()));
 
         ShapeFactory shapeFactory = new ShapeFactory();
 
